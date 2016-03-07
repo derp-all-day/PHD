e2ed9e1851c8113d62e57b07abee745976d2734c
derp-all-day <andrew.blalock.ops@gmail.com>
2016-03-07 12:31:11 -0600

    Create node.php

---
+++ node.php
@@ -0,0 +1,21 @@
+<?php
+include 'phd.class.php';
+
+if(!empty($GLOBALS['_SERVER']['HTTP_USER_AGENT'])) {
+	header('location: ./');
+	die();
+}
+
+if(key($_GET) === 'validate')
+	die(print('valid'));
+
+if(empty($_GET['user']) || empty($_GET['pass']) || empty($_GET['db']))
+	die(print('failure'));
+
+$socket = new PHD($_GET['user'], $_GET['pass'], $_GET['db']);
+
+if(!$socket->isAuthenenticated())
+	die(print('failure'));
+
+echo $socket->nodeInit($_GET['query']);
+?>
